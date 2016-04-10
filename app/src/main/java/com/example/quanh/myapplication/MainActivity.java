6915a26fd0537d118cf48545fcf8c77f8d99b1eb
Minh Quan Hoang <quanhoangminh94@gmail.com>
2016-04-10 15:35:26 +1200

    Some Changes

--- app/src/main/java/com/example/quanh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quanh/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.quanh.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +11,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_HELLO_GUESS = "com.example.quanh.myapplication.HELLO_GUESS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final TextView text = (TextView) findViewById(R.id.textView);
+        final Button but1 = (Button) findViewById(R.id.button);
+        final Button but2 = (Button) findViewById(R.id.button2);
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(but1.getText());
+            }
+        });
+        but2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                if(intent1.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent1);
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
